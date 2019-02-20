1779c2df6f2823b7d5fc8052b84f427cbc5320b8
欧大大 <616559837@qq.com>
2019-02-20 10:08:23 +0800

    main.js

--- src/main.js
+++ src/main.js
@@ -3,13 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import store from '@/libraries/store'
+import Vuex from 'vuex'
+import plugins from '@/libraries/plugins'
+import './assets/styles/main.scss'
+import './views/main/style.scss'
 Vue.config.productionTip = false
+Vue.use(Vuex)
+Vue.use(plugins)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
